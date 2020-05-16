16cda79bef4dc5cf48a5ac8ab3f21b0331bb171f
Ayufasakii <asdwasdwqe@gmail.com>
2020-05-16 13:48:41 +0700

    navbar

---
+++ components/logoMFU.vue
@@ -0,0 +1,20 @@
+<template>
+  <img
+    class="VuetifyLogo"
+    alt="Vuetify Logo"
+    src="../assets/pic/MFU"
+  >
+</template>
+
+<style>
+.VuetifyLogo {
+  height:180px;
+  width: 180px;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateY(0deg);
+  }
+}
+</style>
