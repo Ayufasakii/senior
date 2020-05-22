c5713a8438eee7fc1c7c594ec8749e99a2da46de
Ayufasakii <asdwasdwqe@gmail.com>
2020-05-22 23:07:11 +0700

    fix problem of db

--- server/server.js
+++ server/server.js
@@ -1,28 +1,50 @@
 var mysql = require('mysql');
-var con = mysql.createConnection({
+//////////////////////////////
+var db_config = {
   host: "us-cdbr-east-06.cleardb.net",
   user: "be8afdb670f4c6",
   password:"b0cd0b99",
   database: "heroku_59117959c6d6ee7"
-});
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 //////////////////////////////
 const express = require('express')
 const app = express()
 var cors = require('cors')
 app.use(cors())
 app.get('/getIDandPass', (req, res) => {
-  con.query("SELECT I_ID,I_password FROM internshipstaff", function (err, result, fields) {
+  connection.query("SELECT I_ID,I_password FROM internshipstaff", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
     res.send(result)
   });
 })
 app.get('/getAllStudents', (req, res) => {
-  con.query("SELECT * FROM student", function (err, result, fields) {
+  connection.query("SELECT * FROM student", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
     res.send(result)
