a3b65bd076977edfa65f5754a5328eca672f7544
Ayufasakii <asdwasdwqe@gmail.com>
2020-05-29 14:40:35 +0700

    Update TVF.vue

--- pages/TVF.vue
+++ pages/TVF.vue
@@ -74,6 +74,7 @@ export default {
     data() {
         return {
             search: '',
+            picker: new Date().toISOString().substr(0, 10),
             headers: [{
                     text: 'Student ID',
                     align: 'start',
